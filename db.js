5386ecd46cac822eb72e555620abdb8c4cda2f4e
Ilyes NEDJAI <89983630+ilyesned@users.noreply.github.com>
2023-11-23 11:06:17 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  process.env.key_db, // TODO: database connection string
+  process.env.KEY_DB, // TODO: database connection string
   {
     dialect: 'postgres',
     dialectOptions: {
